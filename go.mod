64f1f7778119819c75eb6a4b946a2e43d452227c
Jaroslav Hadzima <jaroslav.hadzima@thermofisher.com>
2023-12-03 15:07:52 +0100

    new router

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require github.com/fatih/color v1.16.0
 
 require (
+	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	golang.org/x/sys v0.14.0 // indirect
---
+++ routers.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// URLRoute defines the name, method, pattern and handler for routing
+type URLRoute struct {
+	Name           string
+	MethodType     string
+	Pattern        string
+	HandlerFunctor http.HandlerFunc
+}
+
+// URLRoutes is a collection of all available URL routes
+type URLRoutes []URLRoute
+
+// URLRouter defines the required methods for retrieving api routes
+type URLRouter interface {
+	URLRoutes() URLRoutes
+}
+
+// LogHandlerFuncWrapper is a simple wrapper that is able to log every function call
+type LogHandlerFuncWrapper struct {
+	handler http.Handler
+	name    string
+}
+
+// ServeHTTP is a LogHandlerFuncWrapper overrie mtod that logs every call
+func (h LogHandlerFuncWrapper) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	fmt.Printf("Func %v called\n", h.name)
+	h.handler.ServeHTTP(writer, request)
+}
+
+// NewLogHandlerFuncWrapper is a constructor method used to create LogHandlerFuncWrapper
+func NewLogHandlerFuncWrapper(handler http.Handler, name string) LogHandlerFuncWrapper {
+	return LogHandlerFuncWrapper{
+		handler: handler,
+		name:    name,
+	}
+}
+
+// NewURLRouter creates a new router for specified routes
+func NewURLRouter(verbose bool, routers ...URLRouter) *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+	for _, urlRouter := range routers {
+		for _, urlRoute := range urlRouter.URLRoutes() {
+			var handler http.Handler
+			handler = urlRoute.HandlerFunctor
+			if verbose {
+				handler = NewLogHandlerFuncWrapper(handler, urlRoute.Name)
+			}
+
+			router.Methods(urlRoute.MethodType).
+				Path(urlRoute.Pattern).
+				Name(urlRoute.Name).
+				Handler(handler)
+		}
+	}
+
+	return router
+}

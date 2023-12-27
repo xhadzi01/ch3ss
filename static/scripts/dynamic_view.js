7de54c7269ea4c4c1b44e6270f293c6f36c4a2e4
xhadzi01 <killjeaden52@gmail.com>
2023-12-27 22:46:37 +0100

    removed hardcoded path

--- static/scripts/dynamic_view.js
+++ static/scripts/dynamic_view.js
@@ -1,6 +1,6 @@
 document.getElementById('btn').onclick = function() {
     var val = document.getElementById('imagename').value,
-        src = 'http://localhost:32000/static/' + val +'.svg',
+        src = '/static/' + val +'.svg',
         img = document.createElement('img');
 
     img.src = src;

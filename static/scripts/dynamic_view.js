1a48de2379e3a3fb21a95bd5f5ae8900b330a540
xhadzi01 <killjeaden52@gmail.com>
2024-01-07 23:08:50 +0100

    refactored drawing of figures on the screen

--- static/scripts/dynamic_view.js
+++ static/scripts/dynamic_view.js
@@ -1,5 +1,109 @@
+/////////////////////////////////////////////////////
+// fetch requests
 
-function create_filled_piece_text(image_name, dev_id_str, figure_id_str, isDraggable){
+function get_cookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+function redraw_current_figure_positions() {
+  console.debug('fetching game figure positions')
+  let session_id_str = get_cookie("sessionID")
+  fetch(`/get-game-info/${session_id_str}`, {
+      credentials: "same-origin",
+      mode: "same-origin",
+      method: "GET",
+      headers: { "Content-Type": "application/json" }
+  }).then(resp => {
+      if (resp.status === 200) {
+          return resp.json();
+      } else {
+          throw new Error('Something went wrong');
+      }
+  }).then(dataJson => {
+    console.debug('fetching game figure positions - success')
+    draw_chessboard_figures(dataJson.player1.FigureInfoList, dataJson.player2.FigureInfoList)
+  }).catch(err => {
+      console.log(err);
+  })
+}
+
+/////////////////////////////////////////////////////
+// figure image selection
+
+const figure_type_pawn   = 1
+const figure_type_rook   = 2
+const figure_type_knight = 3
+const figure_type_bishop = 4
+const figure_type_queen  = 5
+const figure_type_king   = 6
+
+function get_player_figure_img(figure_type, player_name) {
+  return `${get_player_figure_type_str(figure_type)}_${get_player_figure_color(player_name)}.svg`
+}
+
+function get_player_figure_type_str(figure_type) {
+  switch (figure_type) {
+    case figure_type_pawn:
+      return `pawn`
+    case figure_type_rook:
+      return `rook`
+    case figure_type_knight:
+      return `knight`
+    case figure_type_bishop:
+      return `bishop`
+    case figure_type_queen:
+      return `queen`
+    case figure_type_king:
+      return `king`
+  }
+}
+
+function get_player_figure_color(player_name) {
+  if (player_name === `player1`) {
+    return `g`
+  } else {
+    return `r`
+  }
+}
+
+/////////////////////////////////////////////////////
+// calculation of full chessboard
+
+function create_chessboard_position_map(player1_figures, player2_figures) {
+  let chessboard_positions = {}
+  // there is 64 chess positions (8x8 grid)
+  for (let i = 1; i <= 64; i++) {
+    chessboard_positions[i] = null
+  }
+
+  // then iterate over player 1 and add figures to position map
+  for (let i = 0; i < player1_figures.length; i++) {
+    player_figure = player1_figures[i]
+    chessboard_positions[player_figure.CurrentFigurePosition] = {
+      figure_id: `figure_player1_${player_figure.FigureIndex}_id`,
+      figure_img_src: get_player_figure_img(player_figure.FigureType, 'player1'),
+    }
+  }
+
+  // then iterate over player 2 and add figures to position map
+  for (let i = 0; i < player2_figures.length; i++) {
+    player_figure = player2_figures[i]
+    chessboard_positions[player_figure.CurrentFigurePosition] = {
+      figure_id: `figure_player2_${player_figure.FigureIndex}_id`,
+      figure_img_src: get_player_figure_img(player_figure.FigureType, 'player2'),
+    }
+  }
+  
+  return chessboard_positions
+}
+
+/////////////////////////////////////////////////////
+// element drawing
+
+function create_filled_piece_text(image_name, dev_id_str, figure_id_str, index, isDraggable){
+  console.debug('create_filled_piece_text - enter')
   var filled_piece_template = `<div class="chessPieceDiv" id="DEV_ID_STR" DIV_ON_DRAG_PLACEHOLDER>
                                   <img class="chessPiece" src="/static/IMAGE_NAME" FIGURE_ON_DRAG_PLACEHOLDER id="FIGURE_ID_STR">
                               </div>`
@@ -7,7 +111,7 @@ function create_filled_piece_text(image_name, dev_id_str, figure_id_str, isDragg
   filled_piece_template = filled_piece_template.replace('IMAGE_NAME', image_name).replace('DEV_ID_STR', dev_id_str).replace('FIGURE_ID_STR', figure_id_str);
 
   if (isDraggable)  {
-    filled_piece_template = filled_piece_template.replace('DIV_ON_DRAG_PLACEHOLDER', 'ondrop="drop(event)" ondragover="allowDrop(event)"').replace('FIGURE_ON_DRAG_PLACEHOLDER', 'draggable="true" ondragstart="drag(event)"');
+    filled_piece_template = filled_piece_template.replace('DIV_ON_DRAG_PLACEHOLDER', 'ondrop="drop(event)" ondragover="allowDrop(event)"').replace('FIGURE_ON_DRAG_PLACEHOLDER', 'draggable="true" ondragstart="drag(event, `aaaa`)"');
   } else {
     filled_piece_template = filled_piece_template.replace('DIV_ON_DRAG_PLACEHOLDER', '').replace('FIGURE_ON_DRAG_PLACEHOLDER', '');
   }
@@ -15,7 +119,8 @@ function create_filled_piece_text(image_name, dev_id_str, figure_id_str, isDragg
   return filled_piece_template
 }
 
-function create_empty_piece_text(dev_id_str, isDraggable){
+function create_empty_piece_text(dev_id_str, index, isDraggable){
+  console.debug('create_empty_piece_text - enter')
   var empty_piece_template = `<div class="chessPieceDiv" id="DEV_ID_STR" DIV_ON_DRAG_PLACEHOLDER></div>`
 
   empty_piece_template = empty_piece_template.replace('DEV_ID_STR', dev_id_str);
@@ -29,105 +134,45 @@ function create_empty_piece_text(dev_id_str, isDraggable){
   return empty_piece_template
 }
 
-
-function refreshBoard(){
+function draw_chessboard_figures(player1_figures, player2_figures) {
+  console.debug('drawing chestboard figures - enter')
+  let position_map = create_chessboard_position_map(player1_figures, player2_figures);
 
   var boardElement = document.getElementById('chess_piece_placement')
+  let html_element_str = ""
+  for (let i = 1; i <= 64; i++) {
+    if (position_map[i] != null) {
+      html_element_str += create_filled_piece_text(position_map[i].figure_img_src, `place_${i}_id`, position_map[i].figure_id, i, true);
+    } else {
+      html_element_str += create_empty_piece_text(`place_${i}_id`, i, true);
+    }
+  }
 
-  boardElement.innerHTML = create_filled_piece_text("pawn_r.svg", "some_id1", "some_fig") + 
-  create_filled_piece_text("pawn_r.svg", "some_id2", "some_id2_fig", true) +
-  create_filled_piece_text("pawn_r.svg", "some_id3", "some_id3_fig", true) +
-  create_filled_piece_text("pawn_r.svg", "some_id4", "some_id4_fig", true) +
-  create_filled_piece_text("pawn_r.svg", "some_id5", "some_id5_fig", true) +
-  create_filled_piece_text("pawn_r.svg", "some_id6", "some_id6_fig", true) +
-  create_filled_piece_text("pawn_r.svg", "some_id7", "some_id7_fig", true) +
-  create_filled_piece_text("pawn_r.svg", "some_id8", "some_id8_fig", true) +
-  create_filled_piece_text("pawn_r.svg", "some_id9", "some_id9_fig", true) +
-  create_filled_piece_text("pawn_r.svg", "some_id10", "some_id10_fig", true) +
-  create_filled_piece_text("pawn_r.svg", "some_id11", "some_id11_fig", true) +
-  create_filled_piece_text("pawn_r.svg", "some_id12", "some_id12_fig", true) +
-  create_filled_piece_text("pawn_r.svg", "some_id13", "some_id13_fig", true) +
-  create_filled_piece_text("pawn_r.svg", "some_id14", "some_id14_fig", true) +
-  create_filled_piece_text("pawn_r.svg", "some_id15", "some_id15_fig", true) +
-  create_filled_piece_text("pawn_r.svg", "some_id16", "some_id16_fig", true) +
-  create_empty_piece_text("some_id17", true) +
-  create_empty_piece_text("some_id18", true) +
-  create_empty_piece_text("some_id19", true) +
-  create_empty_piece_text("some_id20", true) +
-  create_empty_piece_text("some_id21", true) +
-  create_empty_piece_text("some_id22", true) +
-  create_empty_piece_text("some_id23", true) +
-  create_empty_piece_text("some_id24", true) +
-  create_empty_piece_text("some_id25", true) +
-  create_empty_piece_text("some_id26", true) +
-  create_empty_piece_text("some_id27", true) +
-  create_empty_piece_text("some_id28", true) +
-  create_empty_piece_text("some_id29", true) +
-  create_empty_piece_text("some_id30", false) +
-  create_empty_piece_text("some_id31", false) +
-  create_empty_piece_text("some_id32", false) +
-  create_empty_piece_text("some_id33", false) +
-  create_empty_piece_text("some_id34", false) +
-  create_empty_piece_text("some_id35", false) +
-  create_empty_piece_text("some_id36", false) +
-  create_empty_piece_text("some_id37", false) +
-  create_empty_piece_text("some_id38", false) +
-  create_empty_piece_text("some_id39", false) +
-  create_empty_piece_text("some_id40", false) +
-  create_empty_piece_text("some_id41", false) +
-  create_empty_piece_text("some_id42", false) +
-  create_empty_piece_text("some_id43", false) +
-  create_empty_piece_text("some_id44", false) +
-  create_empty_piece_text("some_id45", false) +
-  create_empty_piece_text("some_id46", false) +
-  create_empty_piece_text("some_id47", false) +
-  create_empty_piece_text("some_id48", false) +
-  create_filled_piece_text("pawn_g.svg", "some_id49", "some_id49_fig", false) +
-  create_filled_piece_text("pawn_g.svg", "some_id50", "some_id50_fig", false) +
-  create_filled_piece_text("pawn_g.svg", "some_id51", "some_id51_fig", false) +
-  create_filled_piece_text("pawn_g.svg", "some_id52", "some_id52_fig", false) +
-  create_filled_piece_text("pawn_g.svg", "some_id53", "some_id53_fig", false) +
-  create_filled_piece_text("pawn_g.svg", "some_id54", "some_id54_fig", false) +
-  create_filled_piece_text("pawn_g.svg", "some_id55", "some_id55_fig", false) +
-  create_filled_piece_text("pawn_g.svg", "some_id56", "some_id56_fig", false) +
-  create_filled_piece_text("pawn_g.svg", "some_id57", "some_id57_fig", false) +
-  create_filled_piece_text("pawn_g.svg", "some_id58", "some_id58_fig", false) +
-  create_filled_piece_text("pawn_g.svg", "some_id59", "some_id59_fig", false) +
-  create_filled_piece_text("pawn_g.svg", "some_id60", "some_id60_fig", false) +
-  create_filled_piece_text("pawn_g.svg", "some_id61", "some_id61_fig", false) +
-  create_filled_piece_text("pawn_g.svg", "some_id62", "some_id62_fig", false) +
-  create_filled_piece_text("pawn_g.svg", "some_id63", "some_id63_fig", false) +
-  create_filled_piece_text("pawn_g.svg", "some_id64", "some_id64_fig", false)
+  //console.debug('drawing chestboard figures - raw text:', html_element_str)
+  boardElement.innerHTML = html_element_str
 }
 
-function setChessPieceLocation(location, pieceName){
-  var boardElement = document.getElementById('chess_piece_placement')
-}
+/////////////////////////////////////////////////////
+// auxilary
 
 document.getElementById('btn').onclick = function() {
-    //var val = document.getElementById('imagename').value,
-    //    src = '/static/' + val +'.svg',
-    //    img = document.createElement('img');
-
-    //img.src = src;
-    //document.body.appendChild(img);
-
-    refreshBoard()
-    //document.getElementById('chess_piece_placement').appendChild(img);
-
+  console.debug('onclick callback')
+    redraw_current_figure_positions()
 }
 
-refreshBoard();
 
+redraw_current_figure_positions()
 
+/////////////////////////////////////////////////////
+// events
 
-/* dragability events*/
 function allowDrop(ev) {
   ev.preventDefault();
 }
 
-function drag(ev) {
+function drag(ev, someText) {
   ev.dataTransfer.setData("text", ev.target.id);
+  console.log(someText)
 }
 
 function drop(ev) {
